8756a4d96f969591a1f877089baa81595abe4f0e
Jonathan Trujillo <117537776+stormyy00@users.noreply.github.com>
2025-05-05 20:58:29 -0700

    Update page.tsx

--- src/app/photography/gallery/page.tsx
+++ src/app/photography/gallery/page.tsx
@@ -1,12 +1,24 @@
+// app/photography/gallery/[type]/page.tsx
+
 import Gallery from "@/components/photograohy/gallery";
 import React from "react";
 
-const page = () => {
+// This is required for static export with dynamic route
+export async function generateStaticParams() {
+  return [
+    { type: "portraits" },
+    { type: "events" },
+    { type: "products" },
+    // add all valid types you support
+  ];
+}
+
+const Page = ({ params }: { params: { type: string } }) => {
   return (
     <div>
-      <Gallery />
+      <Gallery type={params.type} />
     </div>
   );
 };
 
-export default page;
+export default Page;
