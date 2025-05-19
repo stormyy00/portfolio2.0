9daeb060a7ab581016660f7803ea9a0623f8dbf1
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-05-18 20:55:35 -0700

    images

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -5,11 +5,18 @@ import plants from "../../public/photography/vision/plants.webp";
 import purple from "../../public/photography/vision/purple.webp";
 import wall from "../../public/photography/vision/wall.webp";
 import jerry from "../../public/photography/portraits/jerry.webp";
+import menthy from "../../public/photography/portraits/menthy.webp";
+import theo from "../../public/photography/vision/theo.webp";
+import river from "../../public/photography/seattle/river.webp";
+import beach from "../../public/photography/landscapes/beach.webp";
+import flower from "../../public/photography/landscapes/flowers.webp";
+import sign from "../../public/photography/vision/sign.webp";
+import cart from "../../public/photography/vision/cart.webp";
 
 export const GALLERY = [
   {
     name: "My Vision",
-    image: [enter, heart, plants, purple, wall],
+    image: [theo, enter, heart, plants, purple, wall, sign, cart],
     link: "me",
     type: "landscape",
   },
@@ -21,13 +28,13 @@ export const GALLERY = [
   },
   {
     name: "Portraits",
-    image: [jerry],
+    image: [jerry, menthy],
     link: "Portraits",
     type: "portrait",
   },
   {
     name: "Landscapes",
-    image: [bigbear],
+    image: [flower, beach],
     link: "landscapes",
     type: "landscape",
   },
@@ -51,7 +58,7 @@ export const GALLERY = [
   },
   {
     name: "Seattle",
-    image: [bigbear],
+    image: [river],
     link: "seattle",
     type: "landscape",
   },
