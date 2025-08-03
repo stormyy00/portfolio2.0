51a1243a096bcb8b221247dba5d8237c9dfc8eac
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-08-02 17:11:41 -0700

    added sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,25 +1,25 @@
 const sitemap = () => {
-  const domain = "https://jonathantrujillo.vercel.app"
+  const domain = "https://jonathantrujillo.vercel.app";
   return [
     {
       url: `${domain}`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: `${domain}/photography`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${domain}/blogs`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 0.5,
     },
-  ]
-}
+  ];
+};
 
 export default sitemap;
